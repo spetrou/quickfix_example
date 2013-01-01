eea7c129c563facc4fefc70990c3bbe1fd80ef36
Savvas Petrou <savvaspetrou@hotmail.com>
2013-01-01 21:47:42 +0000

    Code style
    Changed the code style to how I prefer it (with comments).
    Also added the compiled tasks to the .gitignore file.

--- ordermatch/Application.cpp
+++ ordermatch/Application.cpp
@@ -23,218 +23,258 @@
 #include <quickfix/config.h>
 #include <quickfix/fix50/ExecutionReport.h>
 
-void Application::onLogon( const FIX::SessionID& sessionID ) {}
+void Application::onLogon(const FIX::SessionID& sessionID)
+{ }
 
-void Application::onLogout( const FIX::SessionID& sessionID ) {}
+void Application::onLogout(const FIX::SessionID& sessionID)
+{ }
 
-void Application::fromApp( const FIX::Message& message,
-                           const FIX::SessionID& sessionID )
-throw(FIX::FieldNotFound,
-      FIX::IncorrectDataFormat,
-      FIX::IncorrectTagValue,
-      FIX::UnsupportedMessageType)
+void Application::fromApp(const FIX::Message& message,
+                          const FIX::SessionID& sessionID)
+                          throw(FIX::FieldNotFound,
+                                FIX::IncorrectDataFormat,
+                                FIX::IncorrectTagValue,
+                                FIX::UnsupportedMessageType)
 {
-  crack( message, sessionID );
+  crack(message, sessionID);
 }
 
-void Application::onMessage( const FIX50::NewOrderSingle& message, const FIX::SessionID& )
+void Application::onMessage(const FIX50::NewOrderSingle& message,
+                            const FIX::SessionID&)
 {
-  FIX::SenderCompID senderCompID;
-  FIX::TargetCompID targetCompID;
-  FIX::ClOrdID clOrdID;
-  FIX::Symbol symbol;
-  FIX::Side side;
-  FIX::OrdType ordType;
-  FIX::Price price;
-  FIX::OrderQty orderQty;
-  FIX::TimeInForce timeInForce( FIX::TimeInForce_DAY );
-
-  message.getHeader().getField( senderCompID );
-  message.getHeader().getField( targetCompID );
-  message.get( clOrdID );
-  message.get( symbol );
-  message.get( side );
-  message.get( ordType );
-  if ( ordType == FIX::OrdType_LIMIT )
-    message.get( price );
-  message.get( orderQty );
-  if ( message.isSetField( timeInForce ) )
-    message.get( timeInForce );
-
-  try
-  {
-    if ( timeInForce != FIX::TimeInForce_DAY )
-      throw std::logic_error( "Unsupported TIF, use Day" );
-
-    Order order( clOrdID, symbol, senderCompID, targetCompID,
-                 convert( side ), convert( ordType ),
-                 price, (long)orderQty );
-
-    processOrder( order );
-  }
-  catch ( std::exception & e )
-  {
-    rejectOrder( senderCompID, targetCompID, clOrdID, symbol, side, e.what() );
-  }
-}
-
-void Application::onMessage( const FIX50::OrderCancelRequest& message, const FIX::SessionID& )
-{
-  FIX::OrigClOrdID origClOrdID;
-  FIX::Symbol symbol;
-  FIX::Side side;
-
-  message.get( origClOrdID );
-  message.get( symbol );
-  message.get( side );
-
-  try
-  {
-    processCancel( origClOrdID, symbol, convert( side ) );
-  }
-  catch ( std::exception& ) {}
-}
-
-void Application::onMessage( const FIX50::MarketDataRequest& message, const FIX::SessionID& )
-{
-  FIX::MDReqID mdReqID;
-  FIX::SubscriptionRequestType subscriptionRequestType;
-  FIX::MarketDepth marketDepth;
-  FIX::NoRelatedSym noRelatedSym;
-  FIX50::MarketDataRequest::NoRelatedSym noRelatedSymGroup;
-
-  message.get( mdReqID );
-  message.get( subscriptionRequestType );
-  if ( subscriptionRequestType != FIX::SubscriptionRequestType_SNAPSHOT )
-    throw( FIX::IncorrectTagValue( subscriptionRequestType.getField() ) );
-  message.get( marketDepth );
-  message.get( noRelatedSym );
-
-  for ( int i = 1; i <= noRelatedSym; ++i )
-  {
+    FIX::SenderCompID senderCompID;
+    FIX::TargetCompID targetCompID;
+    FIX::ClOrdID clOrdID;
     FIX::Symbol symbol;
-    message.getGroup( i, noRelatedSymGroup );
-    noRelatedSymGroup.get( symbol );
-  }
+    FIX::Side side;
+    FIX::OrdType ordType;
+    FIX::Price price;
+    FIX::OrderQty orderQty;
+    FIX::TimeInForce timeInForce(FIX::TimeInForce_DAY);
+
+    message.getHeader().getField(senderCompID);
+    message.getHeader().getField(targetCompID);
+    message.get(clOrdID);
+    message.get(symbol);
+    message.get(side);
+    message.get(ordType);
+    if (ordType == FIX::OrdType_LIMIT) {
+        message.get(price);
+    }
+    message.get(orderQty);
+    if (message.isSetField(timeInForce)) {
+        message.get( timeInForce );
+    }
+
+    try {
+        if (timeInForce != FIX::TimeInForce_DAY) {
+            throw std::logic_error("Unsupported TIF, use Day");
+        }
+
+        Order order(clOrdID, symbol, senderCompID, targetCompID,
+                    convert(side), convert(ordType),
+                    price, (long)orderQty);
+
+        processOrder(order);
+    }
+    catch(std::exception & e) {
+        rejectOrder(senderCompID, targetCompID, clOrdID, symbol, side, e.what());
+    }
 }
 
-void Application::updateOrder( const Order& order, char status )
+void Application::onMessage(const FIX50::OrderCancelRequest& message,
+                            const FIX::SessionID&)
 {
-  FIX::TargetCompID targetCompID( order.getOwner() );
-  FIX::SenderCompID senderCompID( order.getTarget() );
+    FIX::OrigClOrdID origClOrdID;
+    FIX::Symbol symbol;
+    FIX::Side side;
 
-  FIX50::ExecutionReport fixOrder
-  ( FIX::OrderID ( order.getClientID() ),
-    FIX::ExecID ( m_generator.genExecutionID() ),
-    FIX::ExecType ( status ),
-    FIX::OrdStatus ( status ),
-    FIX::Side ( convert( order.getSide() ) ),
-    FIX::LeavesQty ( order.getOpenQuantity() ),
-    FIX::CumQty ( order.getExecutedQuantity() ) );
+    message.get(origClOrdID);
+    message.get(symbol);
+    message.get(side);
 
-  fixOrder.setField( FIX::ClOrdID( order.getClientID() ) );
-  fixOrder.setField( FIX::OrderQty( order.getQuantity() ) );
+    try {
+        processCancel(origClOrdID, symbol, convert(side));
+    }
+    catch(std::exception&) {
+    }
+}
 
-  if ( status == FIX::OrdStatus_FILLED ||
-       status == FIX::OrdStatus_PARTIALLY_FILLED )
-  {
-    fixOrder.setField( FIX::LastShares( order.getLastExecutedQuantity() ) );
-    fixOrder.setField( FIX::LastPx( order.getLastExecutedPrice() ) );
-    fixOrder.setField( FIX::ExecType(FIX::ExecType_TRADE) );
-  }
-
-  try
-  {
-    FIX::Session::sendToTarget( fixOrder, senderCompID, targetCompID );
-  }
-  catch ( FIX::SessionNotFound& ) {}}
-
-void Application::rejectOrder
-( const FIX::SenderCompID& sender, const FIX::TargetCompID& target,
-  const FIX::ClOrdID& clOrdID, const FIX::Symbol& symbol,
-  const FIX::Side& side, const std::string& message )
+void Application::onMessage(const FIX50::MarketDataRequest& message,
+                            const FIX::SessionID&)
 {
-  FIX::TargetCompID targetCompID( sender.getValue() );
-  FIX::SenderCompID senderCompID( target.getValue() );
+    FIX::MDReqID mdReqID;
+    FIX::SubscriptionRequestType subscriptionRequestType;
+    FIX::MarketDepth marketDepth;
+    FIX::NoRelatedSym noRelatedSym;
+    FIX50::MarketDataRequest::NoRelatedSym noRelatedSymGroup;
 
-  FIX50::ExecutionReport fixOrder
-  ( FIX::OrderID ( clOrdID.getValue() ),
-    FIX::ExecID ( m_generator.genExecutionID() ),
-    FIX::ExecType ( FIX::ExecType_REJECTED ),
-    FIX::OrdStatus ( FIX::ExecType_REJECTED ),
-    side, FIX::LeavesQty( 0 ), FIX::CumQty( 0 ) );
+    message.get(mdReqID);
+    message.get(subscriptionRequestType);
+    if (subscriptionRequestType != FIX::SubscriptionRequestType_SNAPSHOT) {
+        throw(FIX::IncorrectTagValue(subscriptionRequestType.getField()));
+    }
+    message.get(marketDepth);
+    message.get(noRelatedSym);
 
-  fixOrder.setField( clOrdID );
-  fixOrder.setField( FIX::Text( message ) );
+    for (int i = 1; i <= noRelatedSym; ++i) {
+        FIX::Symbol symbol;
+        message.getGroup(i, noRelatedSymGroup);
+        noRelatedSymGroup.get(symbol);
+    }
+}
 
-  try
-  {
-    FIX::Session::sendToTarget( fixOrder, senderCompID, targetCompID );
-  }
-  catch ( FIX::SessionNotFound& ) {}}
+void Application::updateOrder(const Order& order,
+                              char status)
+{
+    FIX::TargetCompID targetCompID(order.getOwner());
+    FIX::SenderCompID senderCompID(order.getTarget());
+
+    FIX50::ExecutionReport fixOrder (FIX::OrderID(order.getClientID()),
+                                     FIX::ExecID(m_generator.genExecutionID()),
+                                     FIX::ExecType(status),
+                                     FIX::OrdStatus(status),
+                                     FIX::Side(convert(order.getSide())),
+                                     FIX::LeavesQty(order.getOpenQuantity()),
+                                     FIX::CumQty(order.getExecutedQuantity()));
+
+    fixOrder.setField(FIX::ClOrdID(order.getClientID()));
+    fixOrder.setField(FIX::OrderQty(order.getQuantity()));
+
+    if (status == FIX::OrdStatus_FILLED ||
+        status == FIX::OrdStatus_PARTIALLY_FILLED)
+    {
+        fixOrder.setField(FIX::LastShares(order.getLastExecutedQuantity()));
+        fixOrder.setField(FIX::LastPx(order.getLastExecutedPrice()));
+        fixOrder.setField(FIX::ExecType(FIX::ExecType_TRADE));
+    }
+
+    try {
+        FIX::Session::sendToTarget(fixOrder, senderCompID, targetCompID);
+    }
+    catch(FIX::SessionNotFound&) {
+    }
+}
+
+void Application::rejectOrder(const FIX::SenderCompID& sender,
+                              const FIX::TargetCompID& target,
+                              const FIX::ClOrdID& clOrdID,
+                              const FIX::Symbol& symbol,
+                              const FIX::Side& side,
+                              const std::string& message)
+{
+    FIX::TargetCompID targetCompID(sender.getValue());
+    FIX::SenderCompID senderCompID(target.getValue());
+
+    FIX50::ExecutionReport fixOrder(FIX::OrderID(clOrdID.getValue()),
+                                    FIX::ExecID(m_generator.genExecutionID()),
+                                    FIX::ExecType(FIX::ExecType_REJECTED),
+                                    FIX::OrdStatus(FIX::ExecType_REJECTED),
+                                    side,
+                                    FIX::LeavesQty(0),
+                                    FIX::CumQty(0));
+
+    fixOrder.setField(clOrdID);
+    fixOrder.setField(FIX::Text(message));
+
+    try {
+        FIX::Session::sendToTarget( fixOrder, senderCompID, targetCompID );
+    }
+    catch(FIX::SessionNotFound&) {
+    }
+}
 
 void Application::processOrder( const Order& order )
 {
-  if ( m_orderMatcher.insert( order ) )
-  {
-    acceptOrder( order );
-
-    std::queue < Order > orders;
-    m_orderMatcher.match( order.getSymbol(), orders );
-
-    while ( orders.size() )
+    if (m_orderMatcher.insert(order))
     {
-      fillOrder( orders.front() );
-      orders.pop();
+        acceptOrder(order);
+
+        std::queue <Order> orders;
+        m_orderMatcher.match(order.getSymbol(),
+                             orders);
+
+        while (orders.size()) {
+            fillOrder(orders.front());
+            orders.pop();
+        }
     }
-  }
-  else
-    rejectOrder( order );
+    else {
+        rejectOrder(order);
+    }
 }
 
-void Application::processCancel( const std::string& id,
-                                 const std::string& symbol, Order::Side side )
+void Application::processCancel(const std::string& id,
+                                const std::string& symbol,
+                                Order::Side side )
 {
-  Order & order = m_orderMatcher.find( symbol, side, id );
-  order.cancel();
-  cancelOrder( order );
-  m_orderMatcher.erase( order );
+    Order & order = m_orderMatcher.find(symbol, side, id);
+    order.cancel();
+    cancelOrder(order);
+    m_orderMatcher.erase(order);
 }
 
-Order::Side Application::convert( const FIX::Side& side )
+Order::Side Application::convert(const FIX::Side& side) const
 {
-  switch ( side )
-  {
-    case FIX::Side_BUY: return Order::buy;
-    case FIX::Side_SELL: return Order::sell;
-    default: throw std::logic_error( "Unsupported Side, use buy or sell" );
-  }
+    switch (side) {
+        case FIX::Side_BUY: {
+            return Order::buy;
+        }
+        break;
+        case FIX::Side_SELL: {
+            return Order::sell;
+        }
+        break;
+        default: {
+            throw std::logic_error("Unsupported Side, use buy or sell");
+        }
+        break;
+    }
 }
 
-Order::Type Application::convert( const FIX::OrdType& ordType )
+Order::Type Application::convert(const FIX::OrdType& ordType) const
 {
-  switch ( ordType )
-  {
-    case FIX::OrdType_LIMIT: return Order::limit;
-    default: throw std::logic_error( "Unsupported Order Type, use limit" );
-  }
+    switch (ordType) {
+        case FIX::OrdType_LIMIT: {
+            return Order::limit;
+        }
+        break;
+        default: {
+            throw std::logic_error("Unsupported Order Type, use limit");
+        }
+        break;
+    }
 }
 
-FIX::Side Application::convert( Order::Side side )
+FIX::Side Application::convert(Order::Side side) const
 {
-  switch ( side )
-  {
-    case Order::buy: return FIX::Side( FIX::Side_BUY );
-    case Order::sell: return FIX::Side( FIX::Side_SELL );
-    default: throw std::logic_error( "Unsupported Side, use buy or sell" );
-  }
+    switch (side) {
+        case Order::buy: {
+            return FIX::Side(FIX::Side_BUY);
+        }
+        break;
+        case Order::sell: {
+            return FIX::Side(FIX::Side_SELL);
+        }
+        break;
+        default: {
+            throw std::logic_error("Unsupported Side, use buy or sell");
+        }
+        break;
+    }
 }
 
-FIX::OrdType Application::convert( Order::Type type )
+FIX::OrdType Application::convert(Order::Type type) const
 {
-  switch ( type )
-  {
-    case Order::limit: return FIX::OrdType( FIX::OrdType_LIMIT );
-    default: throw std::logic_error( "Unsupported Order Type, use limit" );
-  }
+    switch (type) {
+        case Order::limit: {
+            return FIX::OrdType(FIX::OrdType_LIMIT);
+        }
+        break;
+        default: {
+            throw std::logic_error("Unsupported Order Type, use limit");
+        }
+        break;
+    }
 }
+
--- ordermatch/Application.h
+++ ordermatch/Application.h
@@ -38,60 +38,249 @@
 #include <quickfix/fix50/OrderCancelRequest.h>
 #include <quickfix/fix50/MarketDataRequest.h>
 
-class Application
-      : public FIX::Application,
-        public FIX::MessageCracker
+class Application : public FIX::Application,
+                    public FIX::MessageCracker
 {
-  // Application overloads
-  void onCreate( const FIX::SessionID& ) {}
-  void onLogon( const FIX::SessionID& sessionID );
-  void onLogout( const FIX::SessionID& sessionID );
-  void toAdmin( FIX::Message&, const FIX::SessionID& ) {}
-  void toApp( FIX::Message&, const FIX::SessionID& )
-  throw( FIX::DoNotSend ) {}
-  void fromAdmin( const FIX::Message&, const FIX::SessionID& )
-  throw( FIX::FieldNotFound, FIX::IncorrectDataFormat, FIX::IncorrectTagValue, FIX::RejectLogon ) {}
-  void fromApp( const FIX::Message& message, const FIX::SessionID& sessionID )
-  throw( FIX::FieldNotFound, FIX::IncorrectDataFormat, FIX::IncorrectTagValue, FIX::UnsupportedMessageType );
+    /*!
+     *  Overload for class 'FIX::Application'.
+     */
 
-  // MessageCracker overloads
-  void onMessage( const FIX50::NewOrderSingle&, const FIX::SessionID& );
-  void onMessage( const FIX50::OrderCancelRequest&, const FIX::SessionID& );
-  void onMessage( const FIX50::MarketDataRequest&, const FIX::SessionID& );
+    /*!
+     *  @brief      Callback for Session create.
+     *  @details    Callback function when the FIX Session gets
+     *              created.
+     *  @param  param1  The FIX Session ID.
+     */
+    void onCreate(const FIX::SessionID&)
+    { }
 
-  // Order functionality
-  void processOrder( const Order& );
-  void processCancel( const std::string& id, const std::string& symbol, Order::Side );
+    /*!
+     *  @brief      Callback for Logon.
+     *  @details    Callback function when the FIX Session receives
+     *              a FIX Logon message.
+     *  @param  param1  The FIX Session ID.
+     */
+    void onLogon(const FIX::SessionID& sessionID);
 
-  void updateOrder( const Order&, char status );
-  void rejectOrder( const Order& order )
-  { updateOrder( order, FIX::OrdStatus_REJECTED ); }
-  void acceptOrder( const Order& order )
-  { updateOrder( order, FIX::OrdStatus_NEW ); }
-  void fillOrder( const Order& order )
-  {
-    updateOrder( order,
-                 order.isFilled() ? FIX::OrdStatus_FILLED
-                 : FIX::OrdStatus_PARTIALLY_FILLED );
-  }
-  void cancelOrder( const Order& order )
-  { updateOrder( order, FIX::OrdStatus_CANCELED ); }
+    /*!
+     *  @brief      Callback for Logout.
+     *  @details    Callback function when the FIX Session receives
+     *              a FIX Logout message.
+     *  @param  param1  The FIX Session ID.
+     */
+    void onLogout(const FIX::SessionID& sessionID);
 
-  void rejectOrder( const FIX::SenderCompID&, const FIX::TargetCompID&,
-                    const FIX::ClOrdID& clOrdID, const FIX::Symbol& symbol,
-                    const FIX::Side& side, const std::string& message );
+    /*!
+     *  @brief
+     *  @details
+     *  @param
+     */
+    void toAdmin(FIX::Message&,
+                 const FIX::SessionID&)
+    { }
 
-  // Type conversions
-  Order::Side convert( const FIX::Side& );
-  Order::Type convert( const FIX::OrdType& );
-  FIX::Side convert( Order::Side );
-  FIX::OrdType convert( Order::Type );
+    /*!
+     *  @brief
+     *  @details
+     *  @param
+     */
+    void toApp(FIX::Message&,
+               const FIX::SessionID&)
+               throw(FIX::DoNotSend)
+    { }
 
-  OrderMatcher m_orderMatcher;
-  IDGenerator m_generator;
+    /*!
+     *  @brief
+     *  @details
+     *  @param
+     */
+    void fromAdmin(const FIX::Message&,
+                   const FIX::SessionID&)
+                   throw(FIX::FieldNotFound,
+                         FIX::IncorrectDataFormat,
+                         FIX::IncorrectTagValue,
+                         FIX::RejectLogon)
+    { }
+
+    /*!
+     *  @brief
+     *  @details
+     *  @param
+     */
+    void fromApp(const FIX::Message& message,
+                 const FIX::SessionID& sessionID)
+                 throw(FIX::FieldNotFound,
+                       FIX::IncorrectDataFormat,
+                       FIX::IncorrectTagValue,
+                       FIX::UnsupportedMessageType);
+
+    /*!
+     *  Overload for class 'FIX::MessageCracker'.
+     */
+
+    /*!
+     *  @brief      Callback for NewOrderSingle.
+     *  @details    Callback function when a NewOrderSingle message is
+     *              received. It extracts all the necessary fields from
+     *              the incoming message in order to fully initialize an
+     *              'Order' object and then it call a function to
+     *              process it.
+     *  @param  param1  The incoming NewOrderSingle object.
+     *  @param  param2  The FIX Session ID.
+     */
+    void onMessage(const FIX50::NewOrderSingle&,
+                   const FIX::SessionID&);
+
+    /*!
+     *  @brief
+     *  @details
+     *  @param
+     */
+    void onMessage(const FIX50::OrderCancelRequest&,
+                   const FIX::SessionID&);
+
+    /*!
+     *  @brief
+     *  @details
+     *  @param
+     */
+    void onMessage(const FIX50::MarketDataRequest&,
+                   const FIX::SessionID&);
+
+    // Order functionality
+
+    /*!
+     *  @brief
+     *  @details
+     *  @param
+     */
+    void processOrder(const Order&);
+
+    /*!
+     *  @brief
+     *  @details
+     *  @param
+     */
+    void processCancel(const std::string& id,
+                       const std::string& symbol,
+                       Order::Side);
+
+    /*!
+     *  @brief
+     *  @details
+     *  @param
+     */
+    void updateOrder(const Order&, char status);
+
+    /*!
+     *  @brief
+     *  @details
+     *  @param
+     */
+    void rejectOrder(const Order& order)
+    {
+        updateOrder(order,
+                    FIX::OrdStatus_REJECTED);
+    }
+
+    /*!
+     *  @brief
+     *  @details
+     *  @param
+     */
+    void acceptOrder(const Order& order)
+    {
+        updateOrder(order,
+                    FIX::OrdStatus_NEW);
+    }
+    
+    /*!
+     *  @brief
+     *  @details
+     *  @param
+     */
+    void fillOrder(const Order& order)
+    {
+        updateOrder(order,
+                    order.isFilled() ? FIX::OrdStatus_FILLED
+                                     : FIX::OrdStatus_PARTIALLY_FILLED);
+    }
+
+    /*!
+     *  @brief
+     *  @details
+     *  @param
+     */
+    void cancelOrder(const Order& order)
+    {
+        updateOrder(order,
+                    FIX::OrdStatus_CANCELED);
+    }
+
+    /*!
+     *  @brief
+     *  @details
+     *  @param
+     */
+    void rejectOrder(const FIX::SenderCompID&,
+                     const FIX::TargetCompID&,
+                     const FIX::ClOrdID& clOrdID,
+                     const FIX::Symbol& symbol,
+                     const FIX::Side& side,
+                     const std::string& message);
+
+    // Type conversions
+
+    /*!
+     *  @brief      Convert FIX::Side enumeration.
+     *  @details    Convert FIX::Side enumeration to the internal
+     *              Order::Side equivalent.
+     *  @param  param1  The FIX::Side to convert.
+     *  @return The Order::Side enumeration.
+     */
+    Order::Side convert(const FIX::Side&) const;
+
+    /*!
+     *  @brief      Convert FIX::OrdType enumeration.
+     *  @details    Convert FIX::OrdType enumeration to the internal
+     *              Order::Type equivalent.
+     *  @param  param1  The FIX::OrdType to convert.
+     *  @return The Order::Type enumeration.
+     */
+    Order::Type convert(const FIX::OrdType&) const;
+
+    /*!
+     *  @brief      Convert Order::Side enumeration.
+     *  @details    Convert Order::Side enumeration to the FIX
+     *              FIX::Type equivalent.
+     *  @param  param1  The Order::Side to convert.
+     *  @return The FIX::Side enumeration.
+     */
+    FIX::Side convert(Order::Side) const;
+
+    /*!
+     *  @brief      Convert Order::Type enumeration.
+     *  @details    Convert Order::Type enumeration to the FIX
+     *              FIX::OrdType equivalent.
+     *  @param  param1  The Order::Type to convert.
+     *  @return The FIX::OrdType enumeration.
+     */
+    FIX::OrdType convert(Order::Type) const;
+
+    OrderMatcher m_orderMatcher;
+    IDGenerator m_generator;
 
 public:
-  const OrderMatcher& orderMatcher() { return m_orderMatcher; }
+
+    /*!
+     *  @brief
+     *  @details
+     *  @param
+     */
+    const OrderMatcher& orderMatcher() const
+    {
+        return m_orderMatcher;
+    }
 };
 
 #endif
